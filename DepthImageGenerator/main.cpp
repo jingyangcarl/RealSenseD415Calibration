bc85f96bf0d7c973db1d71b7ffb7e95ddf32796c
Jing Yang <jingyang.carl.work@gmail.com>
2019-11-25 12:43:29 -0800

    save color image and depth image

--- DepthImageGenerator/main.cpp
+++ DepthImageGenerator/main.cpp
@@ -6,26 +6,36 @@ using rs2::depth_sensor;
 using rs2::frame;
 using rs2::frameset;
 using rs2::colorizer;
+using rs2::video_frame;
+using rs2::depth_frame;
 
 #include <iostream>
-#include <map>
-#include <string>
 using std::cout;
 using std::endl;
+#include <map>
 using std::map;
+#include <string>
 using std::string;
+using std::stringstream;
+#include <vector>
 using std::pair;
 using std::vector;
+
+#include <opencv2/opencv.hpp>
+using cv::Mat;
+using cv::Size;
 
 #include "Window.h"
 
 int main() {
 
+	// get current RealSense context
+	rs2::context context;
 	int width = 640;
 	int height = 480;
 	int frameRate = 30;
-	int sizeFactor = 3;
-	window app(width * sizeFactor, height * sizeFactor, "Multi-Camera");
+	int deviceSize = context.query_devices().size();
+	window app(width * deviceSize, height * deviceSize, "Multi-Camera");
 
 	// RealSense D415 Asic Serial Number - Index Number
 	map<string, int> asicSerialIndexMap;
@@ -43,6 +53,7 @@ int main() {
 	asicSerialIndexMap.insert(pair<string, int>("825513022115", 12));
 	asicSerialIndexMap.insert(pair<string, int>("822213022092", 13));
 	asicSerialIndexMap.insert(pair<string, int>("821413021123", 14));
+	asicSerialIndexMap.insert(pair<string, int>("821413020551", 15));
 
 	// RealSense D415 Seerial Number - Index Number
 	map<string, int> serialIndexMap;
@@ -60,9 +71,7 @@ int main() {
 	serialIndexMap.insert(pair<string, int>("822512060704", 12));
 	serialIndexMap.insert(pair<string, int>("821212061615", 13));
 	serialIndexMap.insert(pair<string, int>("821212061280", 14));
-
-	// get current RealSense context
-	rs2::context context;
+	serialIndexMap.insert(pair<string, int>("821212061289", 15));
 
 	// present the basic device information
 	cout << "--- " << context.query_devices().size() << " Camera Detected ---" << endl;
@@ -82,7 +91,7 @@ int main() {
 		// Detect if USB Type Descriptor is 3.0 or higher version
 		if (atof(device.get_info(rs2_camera_info::RS2_CAMERA_INFO_USB_TYPE_DESCRIPTOR)) < 3.0) {
 			cout << "Please check USB Port, it may not be USB 3.0 or higher version" << endl;
-			return -1;
+			// return -1;
 		}
 	}
 
@@ -90,6 +99,7 @@ int main() {
 	map<int, float> indexDepthScaleMap;
 	map<int, colorizer> colorizers;
 	vector<pipeline> pipelines;
+	cout << endl << "--- Camera Initialization ---" << endl;
 	for (auto&& device : context.query_devices()) {
 
 		// initialization
@@ -106,27 +116,64 @@ int main() {
 		// set configuration
 		//config.enable_stream(RS2_STREAM_COLOR, width, height, RS2_FORMAT_RGB8, frameRate);
 		//config.enable_stream(RS2_STREAM_DEPTH, width, height, RS2_FORMAT_Z16, frameRate);
+		//config.enable_stream(RS2_STREAM_INFRARED, 1, width, height, RS2_FORMAT_Y8, frameRate);
+		//config.enable_stream(RS2_STREAM_INFRARED, 2, width, height, RS2_FORMAT_Y8, frameRate);
+		config.enable_all_streams();
 		config.enable_device(serialNumber);
 
 		// start the pipeline
 		pipeline.start(config);
-		cout << "Camera [" << serialNumber << "] streaming pipeline starts..." << endl;
+		cout << "Camera [" << serialNumber << "] streaming pipeline is starting..." << endl;
 		pipelines.push_back(pipeline);
 		colorizers[serialIndexMap[serialNumber]] = colorizer();
 	}
 
 	// Main app loop
 	map<int, frame> render_frames;
+	int framesetCount(0);
+	cout << endl << "--- Running the App ---" << endl;
 	while (app) {
 
-		// collect the new new_frames from all the connected devices
+		// collect the new_frames from all the connected devices
 		vector<frame> new_frames;
+		frameset frameset;
 		for (auto&& pipeline : pipelines) {
-			frameset frameset;
 			if (pipeline.poll_for_frames(&frameset)) {
+
+				// push frames
 				for (const frame& frame : frameset) {
 					new_frames.push_back(frame);
 				}
+
+				// get frames
+				auto colorFrame = frameset.get_color_frame();
+				auto depthFrame = frameset.get_depth_frame();
+				auto infraredFrame_1 = frameset.get_infrared_frame(1);
+				auto infraredFrame_2 = frameset.get_infrared_frame(2);
+
+				// print frames
+				cout << "colorFrame: " << colorFrame.get_profile().unique_id() << endl;
+				cout << "depthFrame: " << depthFrame.get_profile().unique_id() << endl;
+				cout << "infraredFrame_1: " << infraredFrame_1.get_profile().unique_id() << endl;
+				cout << "infraredFrame_2: " << infraredFrame_2.get_profile().unique_id() << endl;
+				//cout << "\x1b[A\x1b[A\x1b[A\x1b[A\x1b[A";
+
+				// save frame
+				if (framesetCount % (frameRate*deviceSize) < deviceSize) {
+					stringstream colorImagePath;
+					colorImagePath << "./Generated/colorImage_" << serialIndexMap[pipeline.get_active_profile().get_device().get_info(rs2_camera_info::RS2_CAMERA_INFO_SERIAL_NUMBER)] << ".png";
+					Mat colorImage(Size(colorFrame.get_width(), colorFrame.get_height()), CV_8UC3, (void*)colorFrame.get_data(), Mat::AUTO_STEP);
+					cv::cvtColor(colorImage, colorImage, cv::COLOR_BGR2RGB);
+					cv::imwrite(colorImagePath.str(), colorImage);
+					cv::cvtColor(colorImage, colorImage, cv::COLOR_RGB2BGR);
+
+					stringstream depthImagePath;
+					depthImagePath << "./Generated/depthImage_" << serialIndexMap[pipeline.get_active_profile().get_device().get_info(rs2_camera_info::RS2_CAMERA_INFO_SERIAL_NUMBER)] << ".tiff";
+					Mat depthImage(Size(depthFrame.get_width(), depthFrame.get_height()), CV_16UC1, (void*)depthFrame.get_data(), Mat::AUTO_STEP);
+					cv::imwrite(depthImagePath.str(), depthImage);
+				}
+
+				framesetCount++;
 			}
 		}
 
